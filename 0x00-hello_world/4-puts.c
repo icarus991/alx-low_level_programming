e6c46788ca1c0e0587fa6a2fc791f2dc794a6c57
icarus991 <saldanisabir@gmail.com>
2023-09-07 05:09:12 -0700

    assember

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Description - This program prints a message using the puts function.
+ *
+ * Return - returns 0 to indicate successful execution.
+*/
+#include <stdio.h>
+	int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\"");
+return (0);
+}
