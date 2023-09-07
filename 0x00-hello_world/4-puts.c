d07b23ab3504d283c7d2a71c5ecd4c1a2ade523a
icarus991 <saldanisabir@gmail.com>
2023-09-07 05:59:08 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-/**
- * main - Entry point
- *
- * Description - This program prints a message using the puts function.
- *
- * Return - returns 0 to indicate successful execution.
-*/
 #include <stdio.h>
-	int main(void)
+/**
+ * main - A C programme that prints a line with puts function
+ * Return: 0 (succes)
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - A C programme that prints a line with printf function
+ * Return: 0 (succes)
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
