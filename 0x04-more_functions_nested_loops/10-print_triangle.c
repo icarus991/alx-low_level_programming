63fede49eca583688c5014fa1ca84def544a99eb
icarus991 <saldanisabir@gmail.com>
2023-09-15 06:19:06 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of size 'size'
+ * @size: the size of the triangle
+*/
+
+void print_triangle(int size)
+{
+	int high, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (high = 1; high <= size; high++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((high + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
