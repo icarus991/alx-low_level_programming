b5e56d59e6fbddb1d4abba2043e55653a745b2e3
icarus991 <saldanisabir@gmail.com>
2023-09-21 02:45:27 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i]
+		dest[i] = src[i];
 
 	while (i < n)
 	{
 		dest[i] = '\0';
-		i++
+		i++;
 	}
 	return (dest);
 }
