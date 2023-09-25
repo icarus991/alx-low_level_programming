edc50277a2e22c4ee90eab1a38aab7a9ddd8fa02
icarus991 <saldanisabir@gmail.com>
2023-09-25 04:10:53 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - Calculates the length of the initial
+ * segment of @s that consists
+ * entirely of characters from @accept.
+ * @s: A pointer to the string to be scanned.
+ * @accept: A pointer to the string containing characters to match.
+ *
+ * Return: The number of characters in the initial
+ * segment of @s that match
+ * any character in @accept.
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int str, spn;
+
+	for (str = 0; s[str] != '\0'; str++)
+	{
+		for (spn = 0; accept[spn] != s[str]; spn++)
+		{
+			if (accept[str] == '\0')
+				return (str);
+		}
+	}
+	return (str);
+}
